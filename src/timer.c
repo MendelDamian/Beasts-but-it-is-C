05363cce54ac7d28c375ea9cce2e2e6822588d29
Damian Mendel <mendeldamian01@gmail.com>
2022-10-12 17:44:50 +0200

    removed keyboard and timer handlers
    Due to huge CPU consumptions

--- src/timer.c
+++ src/timer.c
@@ -1,34 +1,11 @@
-#define _DEFAULT_SOURCE  // To delete compiler's warning.
-#include <sys/time.h>
-#include <time.h>
 #include <unistd.h>
 
 #include "timer.h"
 
-void *timer(void *arguments)
+void update_timer(double *delta_time, struct timeval *last_update)
 {
-    if (arguments == NULL)
-    {
-        return NULL;
-    }
-
-    TIMER_ARGS args = *(TIMER_ARGS *)arguments;
-    if (args.delta_time == NULL || args.running == NULL)
-    {
-        return NULL;
-    }
-
-    struct timeval last_update, now;
-    gettimeofday(&last_update, NULL);
-
-    while (*args.running)
-    {
-        nanosleep((const struct timespec[]){{0, 1000000L}}, NULL);
-
-        gettimeofday(&now, NULL);
-        *args.delta_time += ((now.tv_sec - last_update.tv_sec) * 1000) + ((now.tv_usec - last_update.tv_usec) / 1000);
-        last_update = now;
-    }
-
-    return NULL;
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    *delta_time += ((now.tv_sec - last_update->tv_sec) * 1000) + ((now.tv_usec - last_update->tv_usec) / 1000);
+    *last_update = now;
 }
